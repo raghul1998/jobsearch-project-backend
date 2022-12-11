dd457a2eb32874b1451ae3431bcf5803b484de86
JaiVigneshM <50944351+JaiVigneshM@users.noreply.github.com>
2022-12-10 19:48:46 -0500

    login backend setup

--- controller/users/userDao.js
+++ controller/users/userDao.js
@@ -1,3 +1,6 @@
 import userModel from "./userModel.js";
 
 export const creatUser = (user) => userModel.create(user);
+
+export const findUserByEmailIdPassword = (email, password) =>
+  userModel.find().findOne({ email: email, password: password });

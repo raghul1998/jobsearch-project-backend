0686174528885974c2d975ceb9408be515e3e7ec
raghul suresh <raghul1998@gmail.com>
2022-12-11 00:14:27 -0500

    added text index

--- controller/events/model.js
+++ controller/events/model.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 import schema from "./schema.js";
 
+schema.index({shortDescription: 'text'});
 const model = mongoose.model("EventsModel", schema);
+model.createIndexes();
 
 export default model;

e82618b9f3857e734c1e1c16916f8a4c414b77ba
JaiVigneshM <50944351+JaiVigneshM@users.noreply.github.com>
2022-12-10 20:55:55 -0500

    update posts backend

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
 import UsersController from "./controller/users/usersController.js";
 import TestController from "./controller/test/testController.js";
+import eventsController from "./controller/events/service.js";
 
-const app = express()
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+mongoose.connect(
+  "mongodb+srv://subhankarshah:Subhankar29@cluster0.tg8euh0.mongodb.net/?retryWrites=true&w=majority"
+);
 
-mongoose.connect("mongodb+srv://subhankarshah:Subhankar29@cluster0.tg8euh0.mongodb.net/?retryWrites=true&w=majority");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-UsersController(app)
-TestController(app)
-app.listen(process.env.PORT || 4000)
+UsersController(app);
+eventsController(app);
+TestController(app);
+app.listen(process.env.PORT || 4000);
---
+++ controller/events/schema.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema(
+  {
+    postTime: { type: Date, default: new Date() },
+    userId: String,
+    eventName: String,
+    organiserName: String,
+    timeOfEvent: String,
+    dateOfEvent: Date,
+    location: String,
+    shortDescription: String,
+    longDescription: String,
+    image: String,
+    likes: { type: Number, default: 0 },
+    likedBy: [String],
+    comments: [String],
+  },
+  { collection: "events" }
+);
+
+export default schema;
